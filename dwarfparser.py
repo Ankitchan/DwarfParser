ac5a47e1d3a8f006e99a03b81f3de114767a8bf3
Ankit Chandra <ac6426@nyu.edu>
2017-11-15 22:32:53 -0500

    updated parser to include complex data structures

--- dwarfparser.py
+++ dwarfparser.py
@@ -51,33 +51,74 @@ def process_file(filename):
             # We're interested in the filename...
             print('    name=%s' % top_DIE.get_full_path())
 
-	    #for DIE in CU.iterDIE():
+        #for DIE in CU.iterDIE():
             # Display DIEs recursively starting with top_DIE
             dict_data_type = dict()
             
             die_info_rec(top_DIE, dict_data_type)
             
+            dict_struct_info = dict()
+            get_struct_info(top_DIE, dict_struct_info)
+            
             for dataoffset, typevalue in dict_data_type.items():
                 
                 final_name = typevalue.name
-                while typevalue.value != -1:
+                origval = typevalue.value
+                while origval != -1:
                 
-                    if typevalue.value in dict_data_type:
-                        val = dict_data_type[typevalue.value].value
-                        final_name = dict_data_type[typevalue.value].name + ' ' + final_name
-                        typevalue.value = val
+                    if origval in dict_data_type:
+                        val = dict_data_type[origval].value
+                        final_name = dict_data_type[origval].name + ' ' + final_name
+                        origval = val
 
                 dict_data_type[dataoffset].name = final_name
                 #print(str(dataoffset) + " : "+dict_data_type[dataoffset].name + " : " + str(dict_data_type[dataoffset].value))
 
+            #print(dict_data_type)
             dict_var = dict()
             list_var = []
-            dict_var, list_var = get_list_func(top_DIE, dict_var, list_var, dict_data_type)
+            dict_var, list_var = get_list_func(top_DIE, dict_var, list_var, dict_data_type, dict_struct_info)
+            print()
             print(dict_var)
+            print()
             for ele in list_var:
                 print(ele)
 
-def get_list_func(die, dict_var, list_var, dict_data_type):
+def get_struct_info(die, dict_struct_info, is_struct_child = False,struct_parent_offset = -1):
+    
+    child_offset = -1
+    if die.tag == 'DW_TAG_structure_type':
+        is_struct_child = True
+        for key,attrvalue in die.attributes.items():
+            if key == 'DW_AT_name':
+                struct_parent_offset = attrvalue.offset - 1
+                break
+        
+    elif is_struct_child == True and die.tag == 'DW_TAG_member':
+        for key,attrvalue in die.attributes.items():
+            if key == 'DW_AT_type':
+                child_type = attrvalue.value
+                temp_struct_memlist = list()
+                
+                if struct_parent_offset in dict_struct_info:
+                    temp_struct_memlist = dict_struct_info[struct_parent_offset]
+
+                temp_struct_memlist.append(child_type)
+                dict_struct_info[struct_parent_offset] = temp_struct_memlist
+
+                break
+
+    else:
+        is_struct_child = False
+        struct_parent_offset = -1
+
+    for child in die.iter_children():
+        get_struct_info(child, dict_struct_info, is_struct_child, struct_parent_offset)
+
+
+
+
+def get_list_func(die, dict_var, list_var, dict_data_type, dict_struct_info):
 
     for child in die.iter_children():
         if child.tag == 'DW_TAG_subprogram':
@@ -85,24 +126,31 @@ def get_list_func(die, dict_var, list_var, dict_data_type):
             for key,attrvalue in child.attributes.items():
                 if key == 'DW_AT_name' and attrvalue.value != 'main':
                     for kid in child.iter_children():
-                        dict_var,list_var = get_var_info(kid, dict_var,list_var, dict_data_type)
+                        dict_var,list_var = get_var_info(kid, dict_var,list_var, dict_data_type, dict_struct_info)
 
     return dict_var,list_var
 
-def get_var_info(die, dict_var, list_var, dict_data_type):
+def get_var_info(die, dict_var, list_var, dict_data_type, dict_struct_info):
     var_name = ''
     var_loc = ''
     var_type = ''
+    var_offset = -1
     for key, attrvalue in die.attributes.items():
         if key == 'DW_AT_name':
             var_name = attrvalue.value
+            
         elif key == 'DW_AT_type':
             if attrvalue.value in dict_data_type:
+                var_offset = attrvalue.value
                 var_type = dict_data_type[attrvalue.value].name
                 if var_type in dict_var:
                     dict_var[var_type] = dict_var[var_type] + 1
                 else:
                     dict_var[var_type] = 1
+
+                
+
+                
 
         elif key == 'DW_AT_location':
             loc_list = attrvalue.value
@@ -111,10 +159,40 @@ def get_var_info(die, dict_var, list_var, dict_data_type):
                 hex_loc += hex(dec_loc)[2:]
             var_loc = '0x' + hex_loc
 
+        elif var_offset == -1:
+            var_offset = attrvalue.offset - 1
+
+    #if it is a struct enter all its members
+    if 'struct' in var_type:
+        var_offset = get_offset(var_offset,dict_data_type)
+        put_struct_mem(var_offset, dict_var, dict_data_type, dict_struct_info)
+    
     if len(var_name) > 0: 
         list_var.append(Var_data(name=var_name, type=var_type, location=var_loc))
 
     return dict_var,list_var
+
+def get_offset(offset,dict_data_type):
+    temp = dict_data_type[offset].value
+    curroffset = temp
+    while temp != -1:
+        if temp in dict_data_type:
+            curroffset = temp
+            temp = dict_data_type[temp].value
+    return curroffset
+
+
+def put_struct_mem(offset, dict_var,dict_data_type,dict_struct_info):
+    if offset in dict_struct_info:
+        struct_memlist = dict_struct_info[offset]
+
+        for ele in struct_memlist:
+            if ele in dict_data_type:
+                var_type = dict_data_type[ele].name
+                if var_type in dict_var:
+                    dict_var[var_type] = dict_var[var_type] + 1
+                else:
+                    dict_var[var_type] = 1
 
 def die_info_rec(die, dict_data_type):
     """ A recursive function for showing information about a DIE and its
@@ -147,7 +225,7 @@ def die_info_rec(die, dict_data_type):
             if data_type_offset == -1:
                 data_type_offset = attrvalue.offset - 1
         
-        data_type_name = '*'
+        data_type_name = 'pointer'
         dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
     
     elif die.tag == 'DW_TAG_structure_type':
@@ -164,6 +242,54 @@ def die_info_rec(die, dict_data_type):
         
         
         dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=-1)
+
+    elif die.tag == 'DW_TAG_typedef':
+        for key, attrvalue in die.attributes.items():
+            # if data_type_offset > attrvalue.offset:
+            #     data_type_offset = attrvalue.offset
+
+            if key == 'DW_AT_name':
+                data_type_offset = attrvalue.offset - 1
+                data_type_name = ""
+
+            if key == 'DW_AT_type':
+                data_type_val = attrvalue.value
+        
+            if data_type_offset == -1:
+                data_type_offset = attrvalue.offset - 1
+
+        dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
+
+    elif die.tag == 'DW_TAG_member':
+        
+        for key, attrvalue in die.attributes.items():
+            if key == 'DW_AT_name':
+                data_type_offset = attrvalue.offset - 1
+                data_type_name = attrvalue.value
+
+            if key == 'DW_AT_type':
+                data_type_val = attrvalue.value
+
+            if data_type_offset == -1:
+                data_type_offset = attrvalue.offset - 1
+        #print(str(data_type_offset) + " : " + data_type_name + " : " + str(data_type_val) + str(len(dict_data_type)))        
+        dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
+        #print(str(len(dict_data_type)))
+
+    elif die.tag == 'DW_TAG_formal_parameter':
+        
+        for key, attrvalue in die.attributes.items():
+            if key == 'DW_AT_name':
+                data_type_offset = attrvalue.offset - 1
+                data_type_name = attrvalue.value
+
+            if key == 'DW_AT_type':
+                data_type_val = attrvalue.value
+
+            if data_type_offset == -1:
+                data_type_offset = attrvalue.offset - 1
+        #print(str(data_type_offset) + " : " + data_type_name + " : " + str(data_type_val) + str(len(dict_data_type)))        
+        dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
 
     elif die.tag == 'DW_TAG_typedef':
         for key, attrvalue in die.attributes.items():
@@ -228,7 +354,7 @@ def die_info_rec(die, dict_data_type):
             if data_type_offset == -1:
                 data_type_offset = attrvalue.offset - 1
         
-        data_type_name = "subroutine"
+        data_type_name = "function"
         if data_type_offset != -1:         
             dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
 
@@ -242,10 +368,6 @@ def die_info_rec(die, dict_data_type):
                 
         if data_type_offset != -1:                   
             dict_data_type[data_type_offset] = Data_type(name=data_type_name, value=data_type_val)
-
-
-
-
 
     #child_indent = indent_level + '  '
     for child in die.iter_children():
